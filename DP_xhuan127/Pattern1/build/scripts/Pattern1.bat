dd15ba3e2bdcf8122753f3d19826697446df5bc5
Xiaolou Huang <xiaolou.huang@gmail.com>
2019-03-23 17:11:52 -0700

    update gradle build files

--- DP_xhuan127/Pattern1/build/scripts/Pattern1.bat
+++ DP_xhuan127/Pattern1/build/scripts/Pattern1.bat
@@ -63,10 +63,10 @@ set CMD_LINE_ARGS=%*
 :execute
 @rem Setup the command line
 
-set CLASSPATH=%APP_HOME%\lib\Pattern1.jar
+set CLASSPATH=%APP_HOME%\lib\Pattern1.jar;%APP_HOME%\lib\junit-4.12.jar;%APP_HOME%\lib\guava-27.0.1-jre.jar;%APP_HOME%\lib\hamcrest-core-1.3.jar;%APP_HOME%\lib\failureaccess-1.0.1.jar;%APP_HOME%\lib\listenablefuture-9999.0-empty-to-avoid-conflict-with-guava.jar;%APP_HOME%\lib\jsr305-3.0.2.jar;%APP_HOME%\lib\checker-qual-2.5.2.jar;%APP_HOME%\lib\error_prone_annotations-2.2.0.jar;%APP_HOME%\lib\j2objc-annotations-1.1.jar;%APP_HOME%\lib\animal-sniffer-annotations-1.17.jar
 
 @rem Execute Pattern1
-"%JAVA_EXE%" %DEFAULT_JVM_OPTS% %JAVA_OPTS% %PATTERN1_OPTS%  -classpath "%CLASSPATH%" dp1.Main %CMD_LINE_ARGS%
+"%JAVA_EXE%" %DEFAULT_JVM_OPTS% %JAVA_OPTS% %PATTERN1_OPTS%  -classpath "%CLASSPATH%" main.java.Main %CMD_LINE_ARGS%
 
 :end
 @rem End local scope for the variables with windows NT shell
